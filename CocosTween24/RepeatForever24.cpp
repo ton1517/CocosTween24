79a7d60739e1e85ce35d07d3e3a1b9b03ed21bba
ton <tonton1517@gmail.com>
2015-01-03 09:20:37 +0900

    impl RepeatForever24.

---
+++ CocosTween24/RepeatForever24.cpp
@@ -0,0 +1,20 @@
+#include "RepeatForever24.hpp"
+
+namespace cocosTween24
+{
+RepeatForever24Ptr RepeatForever24::create(cocos2d::Node *target, IInterval24Ptr tween)
+{
+    auto r24 = std::make_shared<RepeatForever24>(target, tween);
+
+    return std::move(r24);
+}
+
+RepeatForever24::RepeatForever24(cocos2d::Node *target, IInterval24Ptr tween) : Player24(this, target), _tween(tween) {}
+
+cocos2d::ActionInterval *RepeatForever24::getAction()
+{
+    auto action = _tween->getAction();
+
+    return cocos2d::RepeatForever::create(action);
+}
+} // namespace
